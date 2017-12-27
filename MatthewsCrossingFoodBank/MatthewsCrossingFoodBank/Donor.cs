3c12afc5b9e6af7daab0a8b55a62a162dd525da7
Miguel-Deniz <miguel391@yahoo.com>
2017-12-26 21:36:56 -0700

    Added file parser.

---
+++ MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/Donor.cs
@@ -0,0 +1,29 @@
+﻿using FileHelpers;
+
+namespace MatthewsCrossingFoodBank
+{
+    /// <summary>
+    /// The mapping class for a Donor.
+    /// </summary>
+    [DelimitedRecord(",")]
+    public class Donor
+    {
+        public string donationID;
+
+        public string firstName;
+
+        public string lastName;
+
+        public string streetAddress;
+
+        public string apartment;
+
+        public string cityTown;
+
+        public string stateProvince;
+
+        public string zipPostalCode;
+
+        public string email;
+    }
+}
---
+++ MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/InputParser.cs
@@ -0,0 +1,16 @@
+﻿using FileHelpers;
+
+namespace MatthewsCrossingFoodBank
+{
+    /// <summary>
+    /// Handles parsing the data of the application.
+    /// </summary>
+    class InputParser
+    {
+        public static Donor[] parseFile(string dataFile)
+        {
+            FileHelperEngine<Donor> engine = new FileHelperEngine<Donor>();
+            return engine.ReadFile(dataFile);
+        }   
+    }
+}
