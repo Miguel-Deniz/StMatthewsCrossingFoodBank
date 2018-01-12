338f64f28820aba74d5334926de67136d50f07a0
Miguel-Deniz <miguel391@yahoo.com>
2018-01-11 23:43:08 -0700

    Added HTML letter generator for monetary donations

--- MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/Program.cs
+++ MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/Program.cs
@@ -17,19 +17,7 @@ namespace MatthewsCrossingFoodBank
         [STAThread]
         static void Main()
         {
-            string emailAddress = "matthewcrossingtest1@gmail.com";
-            string password = "appleorange123";
-            string stmpServer = "smtp.gmail.com";
-            int port = 587;
-            bool enableSSL = true;
-            EmailClient client = new EmailClient(emailAddress, password, stmpServer, port, enableSSL);
 
-            //client.sendEmail("migueldeniz70@gmail.com", "Hello", "This should be in the body of the email.");
-
-            //client.test();
-
-            string body = EmailClient.getHTMLMonetaryDonation();
-            client.sendEmail("migueldeniz70@gmail.com", "Test 2", body);
         }
     }
 }
