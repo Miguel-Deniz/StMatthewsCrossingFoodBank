616a4f87935f6d8bb3c4fe3e3c80fffb9ce3c502
Miguel-Deniz <miguel391@yahoo.com>
2018-01-21 01:12:57 -0700

    Email Client Complete

--- MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/Program.cs
+++ MatthewsCrossingFoodBank/MatthewsCrossingFoodBank/Program.cs
@@ -17,7 +17,29 @@ namespace MatthewsCrossingFoodBank
         [STAThread]
         static void Main()
         {
+            string emailAddress = "matthewcrossingtest1@gmail.com";
+            string password = "appleorange123";
+            string stmpServer = "smtp.gmail.com";
+            int port = 587;
+            bool enableSSL = true;
+            EmailClient client = new EmailClient(emailAddress, password, stmpServer, port, enableSSL);
 
+
+
+            MonetaryDonor donor = new MonetaryDonor();
+
+            donor.firstName = "Jason";
+            donor.lastName = "Gaytan";
+            donor.streetAddress = "911 S. Mill Ave";
+            donor.cityTown = "Tempe";
+            donor.stateProvince = "AZ";
+            donor.zipPostalCode = "85211";
+            donor.salutationGreeting = "Dear Jason";
+            donor.amount = "0.00";
+            donor.donatedOn = DateTime.Now.ToString("MMMM dd, yyyy");
+
+            string body = HTMLLetter.getHTML(donor);
+            client.sendEmail("jasongaytan10@gmail.com", "Test", body);
         }
     }
 }
